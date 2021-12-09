1e1eb3cffaadf9a1c8e40913ab7853e16045e30b
Nic Babineaux <76629649+NicBab@users.noreply.github.com>
2021-12-09 17:12:11 -0600

    fixed typo on events routing in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
         <Route exact path="/" component={Home} />
         <Route path="/about" component={About} />
         <Route path="/contact" component={Contact} />
-        <Route path="/event" component={Events} /> 
+        <Route path="/events" component={Events} /> 
       </Switch>
       <Footer />
     </Router>
