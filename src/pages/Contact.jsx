e6527b723556afe2be7447bfcf89789b4b43a732
Nic Babineaux <76629649+NicBab@users.noreply.github.com>
2021-12-16 13:23:08 -0600

    added background component to efach pages

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import {Background} from '../components/index'
 
 const Contact = () => {
     return (
         <div>
             ContactPg
+            <Background />
         </div>
     )
 }
